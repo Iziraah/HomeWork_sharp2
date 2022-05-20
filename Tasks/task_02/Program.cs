17ee1b2c22292b9236ca7e09e5afe676eccc51f4
Mariia <>
2022-05-20 16:59:26 +0300

    решение второй задачи

---
+++ Tasks/task_02/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+
+Console.Write("Ведите число  ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num > 99 ? num.ToString()[2] : "Третьей цифры нет");
+Console.ReadKey(true);
